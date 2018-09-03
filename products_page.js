2b830f9e9c63cd917c37d813d8cfe066053344fc
AlyonaGladych <41871016+AlyonaGladych@users.noreply.github.com>
2018-09-03 11:11:45 +0300

    Update products_page.js

--- products_page.js
+++ products_page.js
@@ -2,6 +2,7 @@ let HeaderPage = require("./header_page.js");
 
 let addProdLinkLocator = "a.section-body_actions";
 
+//body > app > main > administration > div.container > div > div > projects > div > div.section > div.section__left > div:nth-child(3) > div > div > a > span:nth-child(2)
 
 class ProductsPage {
     constructor() {
