ea78301c7ac15066a2eff6dcf7b1c760b6b57959
AlyonaGladych <41871016+AlyonaGladych@users.noreply.github.com>
2018-09-03 20:33:21 +0300

    Update products_page.js

--- products_page.js
+++ products_page.js
@@ -1,19 +1,16 @@
 let HeaderPage = require("./header_page.js");
 
-let addProdLinkLocator = "a.section-body_actions";
-
-//body > app > main > administration > div.container > div > div > projects > div > div.section > div.section__left > div:nth-child(3) > div > div > a > span:nth-child(2)
+let addProdLinkLocator = "a.section-body__actions";
 
 class ProductsPage {
     constructor() {
         this.header = new HeaderPage();
+        browser.waitForAngularEnabled(false);
     }
 
     getAddProduct() {
         return element(by.css(addProdLinkLocator));
-    }  
+    }   
 }
-
-
 
 module.exports = ProductsPage;
